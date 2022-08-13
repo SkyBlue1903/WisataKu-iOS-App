ea3c31f63824d59f744004bd3d90576392815152
Erlangga Anugrah Arifin <angga1433@gmail.com>
2022-08-13 08:40:40 +0700

    Initial Commit

---
+++ WisataKu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WisataKu
+//
+//  Created by Erlangga Anugrah Arifin on 13/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WisataKu/WisataKuApp.swift
@@ -0,0 +1,17 @@
+//
+//  WisataKuApp.swift
+//  WisataKu
+//
+//  Created by Erlangga Anugrah Arifin on 13/08/22.
+//
+
+import SwiftUI
+
+@main
+struct WisataKuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
